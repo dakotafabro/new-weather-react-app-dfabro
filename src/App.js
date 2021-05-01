f3b201656229e47e518d2cf2c54e70ea351e5e45
dakotafabro <dakotafabrocgu@gmail.com>
2021-05-01 10:02:48 -0700

    Created initial file structure for react app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import DailyGreeting from "./DailyGreeting";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          <DailyGreeting />
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/DailyGreeting.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DailyGreeting() {
+  return <h1>Good morning </h1>;
+}
